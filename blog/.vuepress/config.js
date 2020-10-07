37124d85b78d5b8e6ba6e8023fc2727b3df4c2a2
lirc572 <lirc572@gmail.com>
2020-10-07 16:59:32 +0000

    update vuepress config

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,145 +1,146 @@
 module.exports = {
-    title: 'lirc572\'s Blog',
-    description: 'lirc572\'s blog',
-    theme: '@vuepress/theme-blog',
-    themeConfig: {
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#dateformat
-       */
-  
-      dateFormat: 'ddd YYYY-MM-DD',
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#nav
-       */
-  
-      nav: [
+  title: 'lirc572\'s Blog',
+  description: 'lirc572\'s blog',
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#dateformat
+     */
+    dateFormat: 'ddd YYYY-MM-DD',
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#nav
+     */
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+      {
+        text: 'About',
+        link: '/about/',
+      }
+    ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#footer
+     */
+    footer: {
+      contact: [
         {
-          text: 'Home',
+          type: 'github',
+          link: 'https://github.com/lirc572',
+        },
+        {
+          type: 'linkedin',
           link: '/',
         },
         {
-          text: 'Tags',
-          link: '/tag/',
-        },
-      ],
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#footer
-       */
-      footer: {
-        contact: [
-          {
-            type: 'github',
-            link: 'https://github.com/lirc572',
-          },
-          {
-            type: 'linkedin',
-            link: '/',
-          },
-          {
-            type: 'mail',
-            link: 'mailto:lirc572@gmail.com',
-          }
-        ],
-        copyright: [
-          {
-            text: 'Powered by VuePress | lirc572 © 2020-present',
-            link: '/',
-          },
-        ],
-      },
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#directories
-       */
-  
-      directories:[
-        {
-          id: 'post',
-          dirname: '_posts',
-          path: '/',
-          itemPermalink: '/:year/:month/:day/:slug',
+          type: 'mail',
+          link: 'mailto:lirc572@gmail.com',
         }
       ],
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#frontmatters
-       */
-  
-      // frontmatters: [
-      //   {
-      //     id: "tag",
-      //     keys: ['tag', 'tags'],
-      //     path: '/tag/',
-      //   },
-      //   {
-      //     id: "location",
-      //     keys: ['location'],
-      //     path: '/location/',
-      //   },
-      // ],
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#globalpagination
-       */
-  
-      // globalPagination: {
-      //   lengthPerPage: 10,
-      // },
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#sitemap
-       */
-      sitemap: {
-        hostname: 'https://blog.lirc572.xyz/'
-      },
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#comment
-       */
-      comment: {
-        service: 'disqus',
-        shortname: 'lirc572\'s blog',
-      },
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#newsletter
-       */
-      // newsletter: {
-      //   endpoint: 'https://billyyyyy3320.us4.list-manage.com/subscribe/post?u=4905113ee00d8210c2004e038&amp;id=bd18d40138'
-      // },
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#feed
-       */
-      // feed: {
-      //   canonical_base: 'https://example.vuepress-theme-blog.ulivz.com/',
-      // },
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#summary
-       */
-  
-      // summary:false,
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#summarylength
-       */
-  
-      // summaryLength:100,
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#pwa
-       */
-  
-      pwa:true,
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#paginationcomponent
-       */
-  
-      // paginationComponent: 'SimplePagination'
-  
-      /**
-       * Ref: https://vuepress-theme-blog.ulivz.com/config/#smoothscroll
-       */
-      smoothScroll: true
+      copyright: [
+        {
+          text: 'Powered by VuePress | lirc572 © 2020-present',
+          link: '',
+        },
+      ],
     },
-  }
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#directories
+     */
+    directories: [
+      {
+        id: 'post',
+        dirname: '_posts',
+        path: '/',
+        itemPermalink: '/:year/:month/:day/:slug',
+      }
+    ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#frontmatters
+     */
+    // frontmatters: [
+    //   {
+    //     id: "tag",
+    //     keys: ['tag', 'tags'],
+    //     path: '/tag/',
+    //   },
+    //   {
+    //     id: "location",
+    //     keys: ['location'],
+    //     path: '/location/',
+    //   },
+    // ],
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#globalpagination
+     */
+    // globalPagination: {
+    //   lengthPerPage: 10,
+    // },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#sitemap
+     */
+    sitemap: {
+      hostname: 'https://blog.lirc572.xyz/'
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#comment
+     */
+    comment: {
+      service: 'disqus',
+      shortname: 'lirc572',
+    },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#newsletter
+     */
+    // newsletter: {
+    //   endpoint: 'https://billyyyyy3320.us4.list-manage.com/subscribe/post?u=4905113ee00d8210c2004e038&amp;id=bd18d40138'
+    // },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#feed
+     */
+    // feed: {
+    //   canonical_base: 'https://example.vuepress-theme-blog.ulivz.com/',
+    // },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#summary
+     */
+    // summary:false,
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#summarylength
+     */
+    // summaryLength:100,
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#pwa
+     */
+    pwa: {
+      serviceWorker: true,
+      updatePopup: true,
+    },
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#paginationcomponent
+     */
+
+    // paginationComponent: 'SimplePagination'
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/config/#smoothscroll
+     */
+    smoothScroll: true
+  },
+}
